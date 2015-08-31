ecc58c8dfce03d6f5cab26393b15fdf95c33414c
Kyle Huey <khuey@kylehuey.com>
2015-08-31 15:03:17 -0700

    Bug 1200004: Tests. r=baku

---
+++ dom/indexedDB/test/bug1200004.js
@@ -0,0 +1,5 @@
+onconnect = function(event) {
+    var dbreq0 = indexedDB.open("TestDb1", {version:2, storage: "persistent"});
+    close();
+    if (nonexistentvariable != undefined) { var whatever; }
+}
